2fe6149f13edda6988b1ff40d3571e5254269857
pannunzio <mariana_queiroz@yahoo.com>
2016-10-23 21:29:26 -0200

    Novo sprite Açaí

--- src/Acai.cpp
+++ src/Acai.cpp
@@ -1,7 +1,7 @@
 #include "Acai.h"
 
 Acai::Acai(int layer, int subLayer){
-    this->sp = Sprite("img/ggliko.png", 6, 0.09);
+    this->sp = Sprite("img/acai.png", 6, 0.09);
     this->captureSound.Open("audio/comida_getitem.wav", 5);
     SetAssets(layer, subLayer);
 }
